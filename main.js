9b7b0e4109c4e62003246f167ba33ebebd3002db
Piekarz20 <102181834+Piekarz20@users.noreply.github.com>
2024-02-12 09:35:24 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-const addUserBtn = document.querySelector('.addUser');
-const form = document.querySelector('form');
-const actualErr = document.querySelector('.err');
-const bodyMainSection = document.querySelector('.body-main-section');
+const addUserBtn = document.querySelector('#addUser');
+const dane = document.querySelector('#dane');
+const deleteAll = document.querySelector('#deleteAll');
+const errDisplay = document.querySelector('#errDisplay')
 
 addUserBtn.addEventListener('click', (evt) => {
     evt.preventDefault();
-    const userAge = document.querySelector('.userAge').value;
-    const userName = document.querySelector('.userName').value;
+    const userAge = document.querySelector('#userAge').value;
+    const userName = document.querySelector('#userName').value;
 
     if (userAge > 0 && userName.length > 2) {
+        const actualErr = document.querySelector('.err');
+        if (actualErr) {
+            actualErr.remove();
+        }
         const user = {
             userAge: userAge,
             userName: userName
@@ -19,15 +23,21 @@ addUserBtn.addEventListener('click', (evt) => {
         users.forEach((element) => {
             const li = document.createElement('li');
             li.textContent = `Name: ${element.userName}, Age: ${element.userAge}`;
-            bodyMainSection.appendChild(li);
+            dane.appendChild(li);
         });
     } else {
+        const actualErr = document.querySelector('.err');
         if (actualErr) {
             actualErr.remove();
         }
         const err = document.createElement('p');
         err.textContent = 'Podaj prawidłowe dane';
+        err.style.color = '#ff0000'
         err.classList.add('err');
-        form.append(err);
+        errDisplay.append(err);
     }
 });
+deleteAll.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    dane.textContent = ''
+})
